e3ea879faaeeb4652e57c81a32ad3036e608a71f
Kacper <kacperu770@gmail.com>
2020-04-11 08:53:33 +0200

    Fix: Export xls - Now, the generated file contains only the tasks from the current month.

--- model/exportxlsfile.php
+++ model/exportxlsfile.php
@@ -6,7 +6,10 @@ class ExportXLS extends DBconnect{
 
         $pdo= $this->connect();
 
-        $query = $pdo->query("SELECT task_name, work_time FROM time WHERE project_id = '$projectId'");
+        $month = date("m");
+        $year = date("Y");
+
+        $query = $pdo->query("SELECT task_name, work_time FROM time WHERE project_id = '$projectId' AND MONTH(FROM_UNIXTIME(add_date)) = '$month' AND YEAR(FROM_UNIXTIME(add_date))= '$year'");
         $numRows = $query->rowCount();
 
         if ($numRows > 0) {
